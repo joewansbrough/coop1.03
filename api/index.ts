755516ae407627b0fccd3ab83f767160034b0d9b
Joe Wansbrough <joewansbrough@gmail.com>
2026-03-26 12:39:42 -0700

    fix: Restore 50mb body limit in Vercel API handler

--- api/index.ts
+++ api/index.ts
@@ -23,7 +23,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use((req, res, next) => {
   cookieSession({
     name: 'session',
